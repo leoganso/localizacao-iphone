795a461d49917307413580e47db75c50c127971b
leoganso <laboratorioganso@gmail.com>
2025-11-18 14:09:47 -0300

    Update server.js

--- localizacao/server.js
+++ localizacao/server.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Servir arquivos estáticos
+app.use(express.static('.'));
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota de login
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sign-in.html'));
+});
+
+// Processar formulário de login
+app.post('/submit-login', (req, res) => {
+    // Aqui você processa os dados do formulário
+    console.log('Dados recebidos:', req.body);
+    res.redirect('/success.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
